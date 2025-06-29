dbbb217641b8f623bc338cefcbfd10f562c28d8a
Vitaminazz <vitamina.twitu@gmail.com>
2025-06-29 18:43:45 -0300

    Criação da interface ISupplierRepository no Domain

---
+++ StockApp.Domain/Interfaces/ISupplierRepository.cs
@@ -0,0 +1,13 @@
+﻿using StockApp.Domain.Entities;
+
+namespace StockApp.Domain.Interfaces
+{
+    internal interface ISupplierRepository
+    {
+        Task<Supplier> GetByIdAsync(int id);
+        Task<IEnumerable<Supplier>> GetAllAsync();
+        Task AddAsync(Supplier supplier);
+        Task UpdateAsync(Supplier supplier);
+        Task DeleteAsync(int id);
+    }
+}
